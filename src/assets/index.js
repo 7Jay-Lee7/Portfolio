413453ba301ba8f7b6cabe1e6e143c7d596536f4
Jay Lee <126113845+7Jay-Lee7@users.noreply.github.com>
2023-03-12 16:47:07 +0000

    Create index.js

---
+++ src/assets/index.js
@@ -0,0 +1,62 @@
+import logo from "./logo.png";
+import backend from "./backend.png";
+import content from "./content.png";
+import web from "./web.png";
+import github from "./github.png";
+import menu from "./menu.svg";
+import close from "./close.svg";
+import react from "./react.png";
+
+import css from "./tech/css.png";
+import sql from "./tech/sql.png";
+import java from "./tech/java.png";
+import git from "./tech/git.png";
+import html from "./tech/html.png";
+import javascript from "./tech/javascript.png";
+import mongodb from "./tech/mongodb.png";
+import nodejs from "./tech/nodejs.png";
+import reactjs from "./tech/reactjs.png";
+import redux from "./tech/redux.png";
+import tailwind from "./tech/tailwind.png";
+import typescript from "./tech/typescript.png";
+import threejs from "./tech/threejs.svg";
+
+import bq from "./company/b&q.png";
+import greene_king from "./company/greene_king.png";
+import homebase from "./company/homebase.png";
+import rohan from "./company/rohan.png";
+
+import dashboard from "./dashboard.png";
+import servicehub from "./servicehub.png";
+import petlover from "./petlover.png";
+
+export {
+  logo,
+  backend,
+  content,
+  web,
+  github,
+  menu,
+  close,
+  css,
+  sql,
+  java,
+  git,
+  html,
+  javascript,
+  mongodb,
+  nodejs,
+  reactjs,
+  redux,
+  tailwind,
+  typescript,
+  threejs,
+  bq,
+  greene_king,
+  homebase,
+  dashboard,
+  petlover,
+  servicehub,
+  react,
+  rohan,
+};
