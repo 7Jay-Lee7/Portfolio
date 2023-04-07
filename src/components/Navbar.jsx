c1a503ef77c3e1ee267f5ac44f06605990e26220
Jay Lee <126113845+7Jay-Lee7@users.noreply.github.com>
2023-04-07 19:09:07 +0100

    Add files via upload

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -44,7 +44,7 @@ const Navbar = () => {
         >
           <img src={logo} alt='logo' className='w-12 h-12 object-contain' />
           <p className='text-white text-[18px] font-bold cursor-pointer flex'>
-            Jay &nbsp;<span className='sm:block hidden'> | Portfolio</span>
+            Jay Lee &nbsp;<span className='sm:block hidden'> | Portfolio</span>
           </p>
         </Link>
 
