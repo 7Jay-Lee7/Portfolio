577a939f0538c478ef313114a6330054d773a656
Jay Lee <126113845+7Jay-Lee7@users.noreply.github.com>
2023-04-21 21:03:43 +0100

    Add files via upload

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import { BrowserRouter } from "react-router-dom";
 
-import { About, Contact, Experience, /*Feedbacks,*/ Hero, Navbar, Tech, Works, StarsCanvas } from "./components";
+import { About, Contact, Experience, Hero, Navbar, Projects, Tech, StarsCanvas  } from "./components";
 
 const App = () => {
   return (
@@ -14,8 +14,7 @@ const App = () => {
         <About />
         <Experience />
         <Tech />
-        <Works />
-        {/*<Feedbacks /> removed until I have testimonials*/}
+        <Projects />
         <div className='relative z-0'>
           <Contact />
           <StarsCanvas />
